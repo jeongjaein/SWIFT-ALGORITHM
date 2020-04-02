605b1f3fbb50d6b667b50e4f1fe0ddf8b8b7acd6
jeongjaein <wodls96@gmail.com>
2020-04-02 22:37:24 +0900

    완성인둣

--- AL-1FibonacciArray/NumberOfK.swift
+++ AL-1FibonacciArray/NumberOfK.swift
@@ -12,23 +12,31 @@ import Foundation
 
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var answerArray : [Int] = [commands.count]
-    for j in 0...commands.count-1{
-        let arraySlice = array[commands[j][0]-1...commands[j][1]-1]
-        answerArray.append(Array(arraySlice).sorted()[commands[j][2]-1])
+    var answerArray : [Int] = []
+    
+    
+    
+    for j in 0...commands.count - 1{//46432 -> sorted() -> 23446
+        answerArray.append(Array(array[commands[j][0]-1...commands[j][1]-1]).sorted()[commands[j][2]-1])
         sortedArray.removeAll()
         
     }
     return answerArray
 }
 
-var targetArray2 : [Int] = [1,6,5,4,6,4,3,2,1,2,3,4,5,6,7,4,8,0,7,6,5,4,3,2,3,3,1,2,2,3,5,5,6,7,7,8,7,6,5,4,1,3]
-var commandsArray : [[Int]] = [[4,8,5],[6,10,4],[2,10,4],[9,14,3]]
+
+//
+var array : [Int] = [1,6,5,4,6,4,3,2,1,2,3,4,5,6,7,4,8,0,7,6,5]
+//
+var commands : [[Int]] = [[4,8,5],[6,10,4],[2,10,4],[1,8,3],[1,10,6]]
+
+
+
 var sortedArray : [Int] = []
 
 func NumberOfK(){
     
-    print(solution(targetArray2,commandsArray))
+    print(solution(array,commands))
     
     
 }
